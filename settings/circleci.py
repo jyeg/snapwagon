7086bddeaf83f7911bf1185c82bd46bdd3815624
Jason Parent <jason.a.parent@gmail.com>
2017-07-07 01:06:14 -0400

    Updates Circle CI settings

---
+++ settings/circleci.py
@@ -0,0 +1,11 @@
+# Local imports.
+from .local import *
+
+__author__ = 'Jason Parent'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
