29987b42f02f7b593ec000bfb00bd981c5945f3b
Jason Parent <jason.a.parent@gmail.com>
2017-10-15 11:51:59 -0400

    Added index page

--- snapwagon/urls.py
+++ snapwagon/urls.py
@@ -3,6 +3,7 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 __author__ = 'Jason Parent'
 
@@ -11,6 +12,7 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api/', include('organizations.urls', namespace='api')),
     # url(r'^django-rq/', include('django_rq.urls')),
+    url(r'^$', TemplateView.as_view(template_name='index.html'), name='index'),
 ]
 
 if settings.DEBUG:
